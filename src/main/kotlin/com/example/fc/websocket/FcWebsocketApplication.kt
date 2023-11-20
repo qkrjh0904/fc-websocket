43729390a5952d56b3f000aed25a7ae8ba713571
jeongho <qkrjh0904@gmail.com>
2023-11-21 00:01:55 +0900

    git init

---
+++ src/main/kotlin/com/example/fc/websocket/FcWebsocketApplication.kt
@@ -0,0 +1,11 @@
+package com.example.fc.websocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FcWebsocketApplication
+
+fun main(args: Array<String>) {
+    runApplication<FcWebsocketApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/fc/websocket/FcWebsocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.fc.websocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcWebsocketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
